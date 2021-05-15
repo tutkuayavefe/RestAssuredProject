e134345a8d5d0e332330131b1db36295c15c40b7
tutkuayavefe <tutkuayavefe@gmail.com>
2021-05-16 01:33:07 +0200

    post - put - patch - delete methods have been added

--- src/test/java/TestPost.java
+++ src/test/java/TestPost.java
@@ -1,4 +1,7 @@
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
 
 import static org.hamcrest.Matchers.equalTo;
 
@@ -12,12 +15,91 @@ public class TestPost {
 	
 	@Test
 	public void test_post() {
+//		
+//		Map <String, Object> map = new HashMap<String, Object>() ;
+//		
+//		map.put("name", "tutku");
+//		map.put("job", "qa engineer");
+//		System.out.println(map);
 		
-		Map <String, Object> map = new HashMap<String, Object>() ;
 		
-		map.put("name", "tutku");
-		map.put("job", "qa engineer");
-		System.out.println(map);
+		JSONObject request = new JSONObject();
+		request.put("name", "tutku");
+		request.put("job", "qa engineer");
+
+		System.out.println(request);
+		System.out.println(request.toJSONString());
+		
+		given().
+			header("Conten-Type", "application/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			post("https://reqres.in/api/users").
+		then().
+			statusCode(201);
+
+	}
+	
+	@Test
+	public void test_put() {
+
+		
+		JSONObject request = new JSONObject();
+		request.put("name", "tutku");
+		request.put("job", "qa engineer");
+
+		System.out.println(request);
+		System.out.println(request.toJSONString());
+		
+		given().
+			header("Conten-Type", "application/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			put("https://reqres.in/api/users/2").
+		then().
+			statusCode(200).
+			log().all();
+
+	}
+	
+	
+	@Test
+	public void test_patch() {
+
+		
+		JSONObject request = new JSONObject();
+		request.put("name", "tutku");
+		request.put("job", "qa engineer");
+
+		System.out.println(request);
+		System.out.println(request.toJSONString());
+		
+		given().
+			header("Conten-Type", "application/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			patch("https://reqres.in/api/users/2").
+		then().
+			statusCode(200).
+			log().all();
+
+	}
+	
+	@Test
+	public void test_delete() {
+		
+		when().
+			delete("https://reqres.in/api/users/2").
+		then().
+			statusCode(204).
+			log().all();
+
 	}
 
 }
